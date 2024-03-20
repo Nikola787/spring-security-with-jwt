07893119bba98d08da5c0a9e4c9fa3fefc1be32f
nikola787 <ratkovicnikola64@gmail.com>
2024-03-21 00:52:55 +0100

    added project files

---
+++ spring-security-jwt/spring-security-jwt/src/main/java/com/nikola/Security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.nikola.Security.repository;
+
+import com.nikola.Security.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
